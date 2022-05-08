33cb0b654f7ede89465459af5c548da8f5324dea
Abhijay Bidari <craftyryte2011@gmail.com>
2022-05-08 18:57:22 +0530

    Initial commit

---
+++ src/main/java/com/craftyryte/item/ModItems.java
@@ -0,0 +1,4 @@
+package com.craftyryte.item;
+
+public class ModItems {
+}
---
+++ src/main/java/com/craftyryte/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.craftyryte.mixin;
+
+import com.craftyryte.modoffirst.ModOfFirst;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		ModOfFirst.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
